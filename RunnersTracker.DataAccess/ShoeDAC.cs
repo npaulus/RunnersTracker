70fbe06d463e0e37a2202cc3e6c6c4d495bf8dbb
Nate <nate@nate.com>
2013-03-21 13:36:25 -0400

    Add custom class to all forms, finished addactivty form

---
+++ RunnersTracker.DataAccess/ShoeDAC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using RunnersTrackerDB;
+
+namespace RunnersTracker.DataAccess
+{
+    public class ShoeDAC
+    {
+        public ISet<Shoe> GetUserShoes(User user)
+        {
+            var ctx = new RunnersTrackerContext();
+            ISet<Shoe> userShoes = new HashSet<Shoe>();
+            var shoes = ctx.Shoes.Where(s => s.ShoeUserId == user.UserId);
+            foreach (Shoe s in shoes)
+            {
+                userShoes.Add(s);
+            }
+
+            return userShoes;
+        }
+    }
+}
